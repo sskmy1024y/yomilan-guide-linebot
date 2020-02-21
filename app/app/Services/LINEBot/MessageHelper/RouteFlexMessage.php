4d7175f73a689e16c4aea9044edc50efe52ea2ad
sho <sskmy1024.y@gmail.com>
2020-02-22 03:45:56 +0900

    add place and time

--- app/app/Services/LINEBot/MessageHelper/RouteFlexMessage.php
+++ app/app/Services/LINEBot/MessageHelper/RouteFlexMessage.php
@@ -2,20 +2,25 @@
 
 namespace App\Services\LINEBot\MessageHelper;
 
+use Illuminate\Support\Facades\Log;
 use LINE\LINEBot\Constant\Flex\ComponentFontSize;
 use LINE\LINEBot\Constant\Flex\ComponentFontWeight;
 use LINE\LINEBot\Constant\Flex\ComponentLayout;
+use LINE\LINEBot\Constant\Flex\ComponentMargin;
 use LINE\LINEBot\Constant\Flex\ComponentSpacing;
 use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\BoxComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\FillerComponentBuilder;
 use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\TextComponentBuilder;
 use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\BubbleContainerBuilder;
 use LINE\LINEBot\MessageBuilder\FlexMessageBuilder;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use LINE\LINEBot\QuickReplyBuilder;
+use Util_DateTime;
 
 class RouteFlexMessage extends FlexMessageBuilder
 {
-  private $containerBuilder;
+  private $route;
+  private $start_time;
 
   /**
    * @param Route $route
@@ -23,19 +28,23 @@ class RouteFlexMessage extends FlexMessageBuilder
    */
   public function __construct($route, QuickReplyBuilder $quickReply = null)
   {
-    $this->containerBuilder = BubbleContainerBuilder::builder()
-      ->setHeader(self::_getHeader($route->created_at));
+    $this->route = $route;
+    $visit = $this->route->visit;
+
+    $this->start_time = Util_DateTime::createFromYmdHis($visit->start);
+
+    $containerBuilder = BubbleContainerBuilder::builder()
+      ->setHeader(self::_headerComponent())
+      ->setBody(self::_routeBody());
 
     $altText = "よみらん";
-
-    parent::__construct($altText,  $this->containerBuilder, $quickReply);
+    parent::__construct($altText,  $containerBuilder, $quickReply);
   }
 
   /**
-   * @param string $date Y-m-dの形式 
    * @return ComponentBuilder
    */
-  private function _getHeader($date)
+  private function _headerComponent()
   {
     $label = TextComponentBuilder::builder();
     $label->setText("DATE");
@@ -43,7 +52,7 @@ class RouteFlexMessage extends FlexMessageBuilder
     $label->setSize(ComponentFontSize::XS);
 
     $text = TextComponentBuilder::builder();
-    $text->setText($date);
+    $text->setText($this->start_time->Ymd());
     $text->setColor("#ffffff");
     $text->setFlex(4);
     $text->setSize(ComponentFontSize::SM);
@@ -66,5 +75,68 @@ class RouteFlexMessage extends FlexMessageBuilder
     $headerComponentBuilder->setContents([$dateComponent, $titleComponent]);
 
     return $headerComponentBuilder;
+  }
+
+  /**
+   * @return ComponentBuilder
+   */
+  private function _routeBody()
+  {
+    $facilities = $this->route->facilities;
+
+    $placeComponent[] = TextComponentBuilder::builder()
+      ->setText("入園予定時間: " . $this->start_time->format('H:i'))
+      ->setSize(ComponentFontSize::XS)
+      ->setColor("#b7b7b7");
+
+    foreach ($facilities as $facility) {
+      $placeComponent[] = self::_timeAndPlace("00:00", $facility);
+    }
+
+    return BoxComponentBuilder::builder()
+      ->setLayout(ComponentLayout::VERTICAL)
+      ->setContents($placeComponent);
+  }
+
+
+  /**
+   * @param string $time `HH:mm`
+   * @param Facility $facility
+   * @return BoxComponentBuilder
+   */
+  private function _timeAndPlace($time, $facility)
+  {
+    $timeComponent = TextComponentBuilder::builder()
+      ->setText($time)
+      ->setSize(ComponentFontSize::SM);
+
+    $dot = BoxComponentBuilder::builder()
+      ->setWidth("12px")
+      ->setHeight("12px")
+      ->setBorderWidth("2px")
+      ->setCornerRadius("30px")
+      ->setLayout(ComponentLayout::VERTICAL)
+      ->setContents([FillerComponentBuilder::builder()])
+      ->setBorderColor("#6486E3");
+
+    $dotComponent = BoxComponentBuilder::builder()
+      ->setLayout(ComponentLayout::VERTICAL)
+      ->setFlex(0)
+      ->setContents([
+        FillerComponentBuilder::builder(),
+        $dot,
+        FillerComponentBuilder::builder(),
+      ]);
+
+    $placeComponent = TextComponentBuilder::builder()
+      ->setFlex(4)
+      ->setText($facility->name)
+      ->setSize(ComponentFontSize::SM);
+
+    return BoxComponentBuilder::builder()
+      ->setLayout(ComponentLayout::HORIZONTAL)
+      ->setSpacing(ComponentSpacing::LG)
+      ->setMargin(ComponentMargin::XL)
+      ->setContents([$timeComponent, $dotComponent, $placeComponent]);
   }
 }
