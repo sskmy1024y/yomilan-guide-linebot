91663cdf2a3d652223b0a92c126ff24a784a1d0e
sho <sskmy1024.y@gmail.com>
2020-02-25 23:49:18 +0900

    fix noemit error

--- app/resources/assets/ts/app.ts
+++ app/resources/assets/ts/app.ts
@@ -3,9 +3,13 @@ import App from "./app.vue";
 
 require("./bootstrap");
 
-window.Vue = require("vue");
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
 Vue.component("app", App);
 
-const app = new Vue({
-    el: "#app"
-});
+const app = new Vue({});
+app.$mount("#app");
